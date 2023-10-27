3c243d52147def0470fac5212c1464a2cd124ff8
Samuel ManirajSelvaraj <saminkit@gmail.com>
2023-10-27 14:27:57 -0400

    Completed

--- src/test/java/com/serenitydojo/WorkingWithFloatingPointNumbers.java
+++ src/test/java/com/serenitydojo/WorkingWithFloatingPointNumbers.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 public class WorkingWithFloatingPointNumbers {
     @Test
     public void celciusToFarenheit(){
-        double celcius=50.0;
-        double farenheit=0.0;
+        double celcius=50.00;
+        double farenheit=0;
         farenheit= (celcius * 1.8) + 32;
         System.out.println("Temperaure in farenheit is : " +farenheit);
     }
